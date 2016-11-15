810cb2f21929720668186e40d58a5078a3451079
arnarp@gmail.com <LAMAPB@li01r1d.lais.net>
2016-11-15 16:54:44 +0000

    TSLint error fix

--- src/app/counter/counter.cmp.tsx
+++ src/app/counter/counter.cmp.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import {
   createIncrementAction,
-  createDecrementAction
+  createDecrementAction,
 } from './action.creators';
 import {
   IIncrementAction,
-  IDecrementAction
+  IDecrementAction,
 } from './actions';
 import ICounterState from './state';
 const { connect } = require('react-redux');
