f3f0cce775c6c60382b64b0d72c9a7680335e845
Arnar Páll Birgisson <arnarp@gmail.com>
2016-12-14 17:13:14 +0000

    Travis CI#13

--- config/deploy.sh
+++ config/deploy.sh
@@ -14,7 +14,7 @@ if [[ $TRAVIS_BRANCH == 'prod' ]] ; then
 
   # We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD}@${GIT_TARGET}" master:master > /dev/null 2>&1
+  git push --force --quiet "https://${AZURE_WA_USERNAME}:${AZURE_WA_PASSWORD}@${GIT_TARGET}" master:master # > /dev/null 2>&1
 else
   echo 'Invalid branch. You can only deploy from master.'
   exit 1
